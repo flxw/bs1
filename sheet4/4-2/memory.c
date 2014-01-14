698f408581ad74f0a37e7518d59c92175c9e25e8
adi64 <adrian@holfter.de>
2014-01-14 11:49:28 +0100

    ... okay shit, wir sollen die Verwaltungsstrukturen nicht im gleichen Speicher haben -.-

--- sheet4/4-2/memory.c
+++ sheet4/4-2/memory.c
@@ -36,21 +36,59 @@ void memInit(void) {
 void *bs_malloc(size_t size)
 {
     freeEntry* currentEntry = first;
-    while(currentEntry->size < size) {
+    freeEntry* previousFreeEntry = first;
+
+    while(currentEntry->size < (size + sizeof(unsigned int))) {
+        printf("freeEntry @ %p with size %d is too small - using %p\n", currentEntry, currentEntry->size, currentEntry->next);
         if(currentEntry->next == NULL) {
             errno = ENOMEM;
             return NULL;
         }
 
+        previousFreeEntry = currentEntry;
         currentEntry = currentEntry->next;
+        getchar();
     }
 
-    errno=ENOMEM;
-    return NULL;
+    printf("Choosing free block @ %p\n", currentEntry);
+
+    // this position will be returned
+    char* returnPos = ((char*)(currentEntry + 1));
+
+    // create new freeEntry after data block
+    freeEntry* newFreeEntry = (freeEntry*)(returnPos + size + 1);
+    newFreeEntry->next = currentEntry->next;
+    newFreeEntry->size = currentEntry->size - size;
+
+    if(previousFreeEntry == first) {
+        first = newFreeEntry;
+    }else{
+        previousFreeEntry->next = newFreeEntry;
+    }
+
+    printf("New freeEntry @ %p with size %d\n", newFreeEntry, newFreeEntry->size);
+
+    // create block info in front of block
+    unsigned int* dataSize = ((unsigned int*)returnPos) + 1;
+    *dataSize = size;
+
+    printf("dataSize @ %p = %d\n\n", dataSize, *dataSize);
+
+    return (void*)returnPos;
 }
 
 void bs_free(void *ptr)
 {
+    printf("returning data @ %p\n", ptr);
+    unsigned int* dataSize = ((unsigned int*)ptr) - 1);
+    printf("data Size: %d\n", *dataSize);
+
+    // create new freeEntry
+    freeEntry* newFreeEntry = (freeEntry*)dataSize;
+    newFreeEntry->next = first->next;
+    first->next = newFreeEntry;
+    newFreeEntry->size = *dataSize - sizeof(freeEntry);
+
 }
 #endif
   
