ff72fb94d2304dc85a49336ca16d2fcd291848f1
adi64 <adrian@holfter.de>
2014-01-16 12:30:25 +0100

    4-2 Buddy-Algorithmus angefangen

--- sheet4/4-2/memory.c
+++ sheet4/4-2/memory.c
@@ -6,21 +6,10 @@
 #define MEM_SIZE 4096
 
 /* Do not change !! */
+/* ... I did =O */
 static char mem[MEM_SIZE];
 
-
-#ifdef BUDDY
-void *bs_malloc(size_t size) {
-    errno=ENOMEM;
-    return NULL;
-}
-
-void bs_free(void *ptr) {
-}
-#endif 
-
-#ifdef FIRST
-
+/* works for both algorithms */
 typedef struct Node {
     unsigned int startPos;
     unsigned int size;
@@ -29,6 +18,85 @@ typedef struct Node {
     struct Node* prev;
 } Node;
 
+
+#ifdef BUDDY
+
+#define BUDDY_MIN_SIZE 16
+
+Node** nodeListArray;
+unsigned int maxOrder;
+
+void memoryInit(void) {
+    // initialize array of lists
+    maxOrder = 0;
+    unsigned int logReference = BUDDY_MIN_SIZE;
+    while(logReference < MEM_SIZE) {
+        logReference *= 2;
+        maxOrder++;
+    }
+    if(logReference > MEM_SIZE) {
+        printf("MEM_SIZE and BUDDY_MIN_SIZE have to be powers of 2!\n");
+        exit(1);
+    }
+
+    nodeListArray = (Node**)malloc(maxOrder * sizeof(Node*));
+
+    printf("maxOrder: %d\n", maxOrder);
+
+    // initialize nodeListArray
+    unsigned int i=0;
+    for(i=0; i<maxOrder; i++) {
+        nodeListArray[i] = NULL;
+    }
+
+    // initialize root node
+    Node* rootNode = (Node*)malloc(sizeof(Node));
+    nodeListArray[maxOrder - 1] = rootNode;
+
+    rootNode->startPos = 0;
+    rootNode->size = MEM_SIZE;
+    rootNode->free = 1;
+    rootNode->next = NULL;
+    rootNode->prev = NULL;
+}
+
+void memoryCleanup(void) {
+    
+}
+
+
+void *bs_malloc(size_t size) {
+    printf("request for %d bytes\n", size);
+
+    int order = 0;
+    int logReference = BUDDY_MIN_SIZE;
+    while(logReference < size) {
+        logReference *= 2;
+        order++;
+    }
+    if(order > maxOrder) {
+        printf("%d > maxOrder (%d)\n", order, maxOrder);
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    printf("required: order %d or higher\n", order);
+
+    Node* nodePtr = nodeListArray[order];
+    while(nodePtr && nodePtr->next && !nodePtr->free) {
+        
+    }
+
+    errno=ENOMEM;
+    return NULL;
+}
+
+void bs_free(void *ptr) {
+}
+#endif 
+
+
+#ifdef FIRST
 Node* head;
 
 void memoryInit(void) {
@@ -54,7 +122,7 @@ void memoryCleanup(void) {
 void bs_dump(Node* nodePtr) {
     printf("\n");
     while(nodePtr) {
-        printf("%d -> %d (%d), %d, [%p < %p > %p]\n", nodePtr->startPos, nodePtr->startPos + nodePtr->size, nodePtr->size, nodePtr->free, nodePtr->prev, nodePtr, nodePtr->next);
+        printf("%d -> %d (%d), %s free, [%p < %p > %p]\n", nodePtr->startPos, nodePtr->startPos + nodePtr->size, nodePtr->size, nodePtr->free?"":"not", nodePtr->prev, nodePtr, nodePtr->next);
         nodePtr = nodePtr->next;
     }
     printf("\n");
@@ -62,6 +130,8 @@ void bs_dump(Node* nodePtr) {
 
 void *bs_malloc(size_t size) {
     bs_dump(head);
+
+    printf("request for %d bytes\n", size);
 
     Node* nodePtr = head;
     while((!nodePtr->free) || (nodePtr->size < size)) {
@@ -106,8 +176,9 @@ void *bs_malloc(size_t size) {
 }
 
 void bs_free(void *ptr) {
-
     bs_dump(head);
+
+    printf("request to free pointer %p\n", ptr);
 
     long offset = (long)ptr - (long)mem;
     if(offset < 0 || offset > MEM_SIZE) {
